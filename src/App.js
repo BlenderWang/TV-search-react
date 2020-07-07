5e808bcbc8a3dc3ba61dcc296a906a80efb54df8
Shirley Wang <shirleywang527@gmail.com>
2020-07-07 21:44:41 +0200

    add one more row of genres

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SliderWrapper from "./components/sliders/SliderWrapper";
 
 function App() {
     const [items, setItems] = useState([]);
+    const [genres, setGenres] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
@@ -16,9 +17,15 @@ function App() {
                 .sort((a, b) => (a.rating.average > b.rating.average ? -1 : 1))
                 .slice(0, 9);
 
-            // console.log(topTenshows);
+            console.log(topTenshows);
+
+            const genres = result.data
+                .filter((show) => show.genres.includes("Comedy"))
+                .slice(0, 9);
+            console.log(genres);
 
             setItems(topTenshows);
+            setGenres(genres);
             setIsLoading(false);
         };
 
@@ -30,6 +37,21 @@ function App() {
             <Header />
 
             <SliderWrapper isLoading={isLoading} items={items} />
+            <section className="slider-wrapper">
+                <div className="slider__container">
+                    <span>
+                        <i className="fas fa-angle-left"></i>
+                    </span>
+                    {genres.map((genre) => (
+                        <div className="item" key={genre.id}>
+                            <img src={genre.image.medium} alt={genre.name} />
+                        </div>
+                    ))}
+                    <span>
+                        <i className="fas fa-angle-right"></i>
+                    </span>
+                </div>
+            </section>
         </div>
     );
 }
